8ee2a38640756399fc9872345df1f1ac6277ff15
Toby-byte <tobiasbogenielsen@gmail.com>
2024-05-22 10:33:34 +0200

    added rest of code

--- app.py
+++ app.py
@@ -66,6 +66,33 @@ def _():
         print("there was a problem loading the page")
         print(ex)
         return ex
+
+@post("/signup")
+def _():
+    try:
+        username = x.validate_user_username() # validation of username using method from x.py file
+        print("username recived: " + username)
+        email = x.validate_email() # validation of user_last_name using method from x.py file
+        print("email recived: " + email)
+        password = x.validate_password()
+        print("password recived: " + password)
+        ic(username) # this is ice cream it displays error codes when something goes wrong
+        ic(password)
+        ic(email) # this is ice cream it displays error codes when something goes wrong
+        user = {"username":username, "user_email":email, "user_password":password} # defines a user by saving user as a document
+        res = {"query":"INSERT @doc IN users RETURN NEW", "bindVars":{"doc":user}} # inserts a user via AQL query language, via the db method in the x.py file
+        item = x.arango(res)
+        return template("login.html")
+    except Exception as ex:
+        ic(ex)
+        if "user_name" in str(ex):
+            return f"""
+            <template mix-target="#message">
+                {ex.args[1]}
+            </template>
+            """            
+    finally:
+        pass
     
 @post("/users")
 def _():
@@ -188,21 +215,21 @@ def _():
 ##############################
 ##############################
 ##############################
-@post("/signup")
-def _():
-    # password = b'password'
-    # # Adding the salt to password
-    # salt = bcrypt.gensalt()
-    # # Hashing the password
-    # hashed = bcrypt.hashpw(password, salt)
-    # # printing the salt
-    # print("Salt :")
-    # print(salt)
+# @post("/signup")
+# def _():
+#     # password = b'password'
+#     # # Adding the salt to password
+#     # salt = bcrypt.gensalt()
+#     # # Hashing the password
+#     # hashed = bcrypt.hashpw(password, salt)
+#     # # printing the salt
+#     # print("Salt :")
+#     # print(salt)
     
-    # # printing the hashed
-    # print("Hashed")
-    # print(hashed)    
-    return "signup"
+#     # # printing the hashed
+#     # print("Hashed")
+#     # print(hashed)    
+#     return "signup"
 
 
 ##############################

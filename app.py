2c5466bda60f08fcfa1bd407e2fd5fb8302c5caa
Toby-byte <tobiasbogenielsen@gmail.com>
2024-05-29 10:33:12 +0200

    login edit

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ def login():
 
 sessions = {}
 
-@post("/login_arango")
+@post("/login")
 def login_post():
     try:
         user_email = request.forms.get("user_email")
@@ -307,81 +307,6 @@ def _():
 @get("/api")
 def _():
     return x.test()
-
-
-##############################
-##############################
-##############################
-# @post("/signup")
-# def _():
-#     # password = b'password'
-#     # # Adding the salt to password
-#     # salt = bcrypt.gensalt()
-#     # # Hashing the password
-#     # hashed = bcrypt.hashpw(password, salt)
-#     # # printing the salt
-#     # print("Salt :")
-#     # print(salt)
-    
-#     # # printing the hashed
-#     # print("Hashed")
-#     # print(hashed)    
-#     return "signup"
-
-
-# ##############################
-# @post("/login")
-# def _():
-#     try:
-#         user_email = x.validate_email()
-#         user_password = x.validate_password()
-#         db = x.db()
-#         q = db.execute("SELECT * FROM users WHERE user_email = ? LIMIT 1", (user_email,))
-#         user = q.fetchone()
-#         if not user: raise Exception("user not found", 400)
-#         if not bcrypt.checkpw(user_password.encode(), user["user_password"].encode()): raise Exception("Invalid credentials", 400)
-#         user.pop("user_password") # Do not put the user's password in the cookie
-#         ic(user)
-#         try:
-#             import production
-#             is_cookie_https = True
-#         except:
-#             is_cookie_https = False        
-#         response.set_cookie("user", user, secret=x.COOKIE_SECRET, httponly=True, secure=is_cookie_https)
-        
-#         frm_login = template("__frm_login")
-#         return f"""
-#         <template mix-target="frm_login" mix-replace>
-#             {frm_login}
-#         </template>
-#         <template mix-redirect="/profile">
-#         </template>
-#         """
-#     except Exception as ex:
-#         try:
-#             response.status = ex.args[1]
-#             return f"""
-#             <template mix-target="#toast">
-#                 <div mix-ttl="3000" class="error">
-#                     {ex.args[0]}
-#                 </div>
-#             </template>
-#             """
-#         except Exception as ex:
-#             ic(ex)
-#             response.status = 500
-#             return f"""
-#             <template mix-target="#toast">
-#                 <div mix-ttl="3000" class="error">
-#                    System under maintainance
-#                 </div>
-#             </template>
-#             """
-        
-
-#     finally:
-#         if "db" in locals(): db.close()
-
 
 ##############################
 @post("/toogle_item_block")
